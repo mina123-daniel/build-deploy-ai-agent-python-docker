6a4a58331ff0dcae02dc5a3c6fa3e88dd1eb76cc
Mina Ebraheem <mina.ebraheem@ict.nl>
2025-10-29 14:45:56 +0100

    adding the env variables

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,14 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
+MY_PROJECT = os.environ.get("MY_PROJECT") or "This is my project"
+API_KEY = os.environ.get("API_KEY")
+
+if not API_KEY:
+    raise NotImplementedError("'API_KEY' is not set in environment variables")
+
 @app.get("/")
 def read_index():
-    return{"hello":"world!"}
+    return{"hello":"world!", "project name": MY_PROJECT, "api_key": API_KEY}
